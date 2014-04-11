0a17f88fe670260fc71fb7343f465871d05f4200
René Zurita Corro <zc.rene@gmail.com>
2014-04-11 10:20:10 -0500

    update: problem 4 solved.

--- bazaraa_machine_schedule.py
+++ bazaraa_machine_schedule.py
@@ -5,8 +5,6 @@ from itertools import chain
 lp = glpk.LPX()
 lp.name = "Problema de Calendarización de máquinas"
 lp.obj.maximize = False
-# lp.kind = "int"
-# print type(lp.kind)
 
 # Es un problema de asignación, el solver ya no debe ser simplex, sino
 # algo para programación entera
@@ -22,6 +20,11 @@ tiempos = [[0.3, 0.25, 0.2, 0.2 ],
 horas_disp = [1500, 1200, 1500, 2000]
 unidades_req = [4000, 5000, 3000]
 
+# Función objetivo
+func_obj = list(chain.from_iterable(costos))
+
+print "función objetivo", func_obj
+
 # Identificación de las restricciones
 lp.rows.add(3 + 4)
 for elem in range(len(horas_disp)):
@@ -34,8 +37,10 @@ for elem in range(len(unidades_req)):
 # Identificación de las variables
 lp.cols.add(3*4)
 for c in lp.cols:
-    c.name = "P[{0}]M[{1}]".format(c.index / 4 , c.index % 4 )
+    c.name = "P[{0}]M[{1}]".format(c.index / 4 + 1 , c.index % 4 + 1 )
+    c.kind = int
     c.bounds = 0.0, None
+    print c.name + " " + str(func_obj[c.index])
 
 # Declaración de las restricciones
 restricciones = []
@@ -52,34 +57,33 @@ for i in range(len(unidades_req)):
 
 lp.matrix = restricciones
 
-print "**********"
-for row in lp.rows:
-    print "----------"
-    print row.name
-    # print row.bounds
-    message = " + ".join(
-        [str(r[1])+str(lp.cols[r[0]].name) for r in row.matrix])
-    if row.bounds[0] != row.bounds[1]:
-        message = (str(row.bounds[0]) + " <= " + message + " <= " +
-                   str(row.bounds[1]))
-    else:
-        message += " = "+str(row.bounds[0])
-    print message
-print "**********"
-
-# Función objetivo
-# func_obj = list(chain.from_iterable(costos))
-func_obj = [costos[i][j] * tiempos[i][j] for i in range(3) for j in range(4)]
-
-print "función objetivo", func_obj
+# print "**********"
+# for row in lp.rows:
+#     print "----------"
+#     print row.name
+#     # print row.bounds
+#     message = " + ".join(
+#         [str(r[1])+str(lp.cols[r[0]].name) for r in row.matrix])
+#     if row.bounds[0] != row.bounds[1]:
+#         message = (str(row.bounds[0]) + " <= " + message + " <= " +
+#                    str(row.bounds[1]))
+#     else:
+#         message += " = "+str(row.bounds[0])
+#     print message
+# print "**********"
 
 lp.obj[:] = func_obj
 
-# lp.intopt()
-lp.simplex()
-# lp.integer()
-# lp.interior()
+solver_list = [lp.intopt, lp.simplex, lp.integer, lp.interior]
 
-print "Z = {0}".format(lp.obj.value)
-print "\n".join('{0}[indx:{2}] = {1}'.format(c.name, c.primal, c.index) for
-                c in lp.cols)
+for solv in  solver_list:
+    print "**********"
+    print "=========="
+    print "using ", solv
+    print "=========="
+    solv()
+
+    print "Z = {0}".format(lp.obj.value)
+    print "\n".join('{0}[indx:{2}] = {1}'.format(
+        c.name, c.primal, c.index) for c in lp.cols)
+print "**********"
